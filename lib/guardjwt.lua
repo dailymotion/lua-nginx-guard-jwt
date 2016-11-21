f5ba684ca2dbc3570b5b06e28f2aaa723ceeb305
Stan Chollet <stanislas.chollet@gmail.com>
2016-11-21 18:56:16 +0100

    :whale2: Split the usage of GuardJWT in 2 methods.
    ```lua
    GuardJWT.raw_auth(nginx, claims_to_headers_mapping, is_token_mandatory,
    secret)
    ```
    
    ```lua
    GuardJWT.auth(claims_to_headers_mapping, is_token_mandatory, secret)
    ```

--- lib/guardjwt.lua
+++ lib/guardjwt.lua
@@ -7,11 +7,13 @@ local _M = { _VERSION = '0.4' }
 local GuardJWT = {}
 _M.GuardJWT = GuardJWT
 
-function _get_claims(nginx, authorization, secret)
-  if secret == nil then
-    secret = os.getenv("JWT_SECRET")
-  end
 
+--@function Get claims values from a authorization value (private method)
+--@param nginx NGINX object
+--@param authorization Value from header "authorization"
+--@param secret Secret key to decrypt JWT Token
+--@return Claims values (Available in the token's "payload" key)
+function _get_claims(nginx, authorization, secret)
   if authorization == nil then
       nginx.log(nginx.NOTICE, "[JWTGuard] No authorization header")
       return nil
@@ -33,19 +35,33 @@ function _get_claims(nginx, authorization, secret)
   return decoded_jwt['payload']
 end
 
+
+--@function Purge HTTP Request's headers mapped with the claims's values to
+-- avoid header's injections (private method)
+--@param nginx NGINX object
+--@param claims_to_headers_mapping Mapping between claims's values and headers
 function _purge_headers(nginx, claims_to_headers_mapping)
   for _, claim_conf in pairs(claims_to_headers_mapping) do
     nginx.req.clear_header(string.lower(claim_conf.header))
   end
 end
 
-function GuardJWT.auth(nginx, claims_to_headers_mapping, is_token_mandatory)
+
+--@function Raw method to authenticate guest from the Authorization header.
+--@param nginx NGINX object
+--@param claims_to_headers_mapping Mapping configuration between claim's values
+-- and HTTP Request's headers
+--@param is_token_mandatory Set if JWT token must be valid et present,
+-- false by default
+--@param secret Secret used to decrypt JWT Token guess from JWT_SECRET env
+-- variable if not present
+function GuardJWT.raw_auth(nginx, claims_to_headers_mapping, is_token_mandatory, secret)
     assert(type(claims_to_headers_mapping) == 'table', "[JWTGuard] claims_to_headers_mapping is mandatory")
     assert(#claims_to_headers_mapping <= 0, "[JWTGuard] claims_to_headers_mapping should not be empty")
 
     _purge_headers(nginx, claims_to_headers_mapping)
 
-    local claims = _get_claims(nginx, nginx.req.get_headers()["authorization"])
+    local claims = _get_claims(nginx, nginx.req.get_headers()["authorization"], secret)
 
     if is_token_mandatory and type(claims) ~= 'table' and #claims <= 0 then
         nginx.log(nginx.ERR, "[JWTGuard] No Authorization provided")
@@ -63,8 +79,26 @@ function GuardJWT.auth(nginx, claims_to_headers_mapping, is_token_mandatory)
         if claim_value ~= nil then
           nginx.log(nginx.DEBUG, "Add Claim '" .. claim_value .. "' to header '" .. claim_conf.header .. "'")
           nginx.req.set_header(claim_conf.header, claim_value)
+          nginx.req.clear_header("authorization")
         end
     end
+end
+
+
+--@function Method to authenticate guest from the Authorization header.
+-- nginx object if guessed from the global scope
+--@param claims_to_headers_mapping Mapping configuration between claim's values
+-- and HTTP Request's headers
+--@param is_token_mandatory Set if JWT token must be valid et present,
+-- false by default
+--@param secret Secret used to decrypt JWT Token, guessed from JWT_SECRET env
+-- variable if not present
+function GuardJWT.auth(claims_to_headers_mapping, is_token_mandatory, secret)
+    if secret == nil then
+      secret = os.getenv("JWT_SECRET")
+    end
+
+    GuardJWT.raw_auth(ngx, claims_to_headers_mapping, is_token_mandatory, secret)
 end
 
 return _M
